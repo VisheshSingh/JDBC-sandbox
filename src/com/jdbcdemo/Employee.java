dd2b32af12407972d644c7e3f7ed53028b1e4326
Vishesh <visheshsingh16@gmail.com>
2018-02-04 11:42:03 -0600

    Java Swing GUI for searching employee by last name

---
+++ src/com/jdbcdemo/Employee.java
@@ -0,0 +1,65 @@
+package com.jdbcdemo;
+
+public class Employee {
+	private int id;
+	private String first_name;
+	private String last_name;
+	private String email;
+	private double salary;
+
+	public Employee(int id, String first_name, String last_name, String email, double salary) {
+		super();
+		this.id = id;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.email = email;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", first_name=" + first_name + ", last_name=" + last_name + ", email=" + email
+				+ ", salary=" + salary + "]";
+	}
+
+}
